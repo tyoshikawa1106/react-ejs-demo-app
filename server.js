323271561216958411ffcf7063948a03ef0c5f7f
Taiki Yoshikawa <tyoshikawa1106@gmail.com>
2016-05-24 20:53:18 +0900

    server.jsとpackage.json修正

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
 var app = express();
 var port = process.env.PORT || 8080;
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', '/images/favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Require Routes js
 var routesIndex = require('./routes/index');
@@ -21,6 +35,16 @@ app.use('/api', routesApi);
 
 // View Engine To ejs
 app.set('view engine', 'ejs');
+
+app.use(function(req, res, next) {
+    // Set permissive CORS header - this allows this server to be used only as
+    // an API server in conjunction with something like webpack-dev-server.
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Disable caching so we'll always get the latest comments.
+    res.setHeader('Cache-Control', 'no-cache');
+    next();
+});
 
 // Served Localhost
 console.log('Served: http://localhost:' + port);
